b2c8355e56bc4adb3b4559e3826e14b96627f2c4
Gulzat Karimova and Manasa Vesala <gulzat@>
2019-01-23 11:37:47 -0800

    recipe box and added images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Recipe} from './models/recipe.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  display:boolean = false;
+ recipies:Recipe[] = [
+   new Recipe('Fried eggs',['egg','salt','butter'],'put butter in a pan and add eggs into pan,add salt,done.','./assets/images/eggs.jpeg'),
+   new Recipe('pasta',['pasta','salt','tomatoes'],'Add pasta in boiling water,Add salt and tomatoes,done.', './assets/images/pasta.jpg')
+ ];
+
+ showRecipe(){
+ this.display=!this.display;
+}
+public selectedRecipe: Recipe = null;
+// editRecipe(recipe:Recipe){
+//   this.selectedRecipe = recipe;
+// }
+// finishedEditing(){
+//   this.selectedRecipe = null;
+// }
 }
